32f36bafb953006fa235b16417d58c7da08560ab
zlic <161250197@smail.nju.edu.cn>
2022-03-07 14:33:14 +0800

    504.七进制数

---
+++ 504.七进制数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=javascript
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+  return num.toString(7);
+};
+// @lc code=end
+
